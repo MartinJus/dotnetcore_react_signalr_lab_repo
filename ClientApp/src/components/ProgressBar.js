3a3dbf088202cdea77712317ba2374b2ad401b87
Martin Johansson <martin.johansson17@gmail.com>
2020-01-23 16:19:13 +0100

    ProgressBar is now a bit nices than before

--- ClientApp/src/components/ProgressBar.js
+++ ClientApp/src/components/ProgressBar.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { Progress } from "reactstrap";
 
 export const ProgressBar = props => (
     <p>
-        [
-            {
-                Array(100).fill('_').fill('x', 0, props.progress).map(val=> (
-                    <span>{val}</span>
-                ))
-            }
-        ]
+        <div className="progress-outer">
+            <div className="progress-inner" style={{ width: `${props.progress}%`}}></div>
+        </div>
     </p>
 )
