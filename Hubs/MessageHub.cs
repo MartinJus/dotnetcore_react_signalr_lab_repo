9013b755ecca96c73edf45086618badabdeca91d
Martin Johansson <martin.johansson17@gmail.com>
2020-01-24 10:53:09 +0100

    Did some cleaning in SignalR Hub

--- Hubs/MessageHub.cs
+++ Hubs/MessageHub.cs
@@ -4,17 +4,8 @@ using System.Threading.Tasks;
 
 namespace SignalR.Hubs
 {
-    public class MessageHub : Hub//, IMessageHub
+    public class MessageHub : Hub
     {
-        // public MessageHub() {
-            
-        // }
-
-        // public override async Task OnConnectedAsync() {
-        //     // Console.WriteLine($"Client with id {Context.ConnectionId} connected.");
-        //     await OnConnectedAsync();
-        // }
-
         public async Task SendMessage(string message)
         {
             Console.WriteLine($"Should send message {message}");
@@ -25,6 +16,7 @@ namespace SignalR.Hubs
 
         public async Task UpdateJobProgress(string connectionId, string jobId, int progress) 
         {
+            Console.WriteLine($"Sending update job to {connectionId} for job {jobId}, progress is: {progress}");
             await Clients.Client(connectionId).SendAsync("UpdateJobProgress", jobId, progress);
         }
     }
