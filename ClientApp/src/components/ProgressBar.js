8b8bd7e8a75eb1512bd9f154019134415b1e689c
Martin Johansson <martin.johansson17@gmail.com>
2020-01-24 10:56:18 +0100

    Added percentage text to the progress bar.

--- ClientApp/src/components/ProgressBar.js
+++ ClientApp/src/components/ProgressBar.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export const ProgressBar = props => (
-    <p>
+export const ProgressBar = (props) => {
+    return ( 
         <div className="progress-outer">
+            <span className="progress-text">{props.progress}%</span>
             <div className="progress-inner" style={{ width: `${props.progress}%`}}></div>
         </div>
-    </p>
-)
+    );
+}
