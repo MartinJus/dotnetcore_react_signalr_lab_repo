85866a7e2f8bb1c3eafa0394ed8c06f99f507df3
Martin Johansson <martin.johansson17@gmail.com>
2020-01-27 15:40:22 +0100

    Added useState() call to ProgressBar in order to increment counter by one even though backend reports progress with an increase of 10.

--- ClientApp/src/components/ProgressBar.js
+++ ClientApp/src/components/ProgressBar.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const ProgressBar = (props) => {
+    const [counter, setCounter] = useState(0);
+    if (counter < props.progress) {
+        setTimeout(() => {
+            setCounter(counter + 1);
+        }, 50);
+    }
     return ( 
         <div className="progress-outer">
-            <span className="progress-text">{props.progress}%</span>
-            <div className="progress-inner" style={{ width: `${props.progress}%`}}></div>
+            <span className="progress-text">{counter}%</span>
+            <div className="progress-inner" style={{ width: `${counter}%`}}></div>
         </div>
     );
 }
